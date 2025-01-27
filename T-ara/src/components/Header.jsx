2ffdc9123e3893ba425b5a266a6df6eb4664a7d6
박주찬 <p990805@naver.com>
2025-01-27 19:16:11 +0900

    [주찬] Design : navbar 수정하고  login 페이지디자인 01.27

--- T-ara/src/components/Header.jsx
+++ T-ara/src/components/Header.jsx
@@ -1,43 +1,81 @@
-import "./styles/Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className="flex justify-between items-center px-8 py-4 bg-white shadow-md">
       {/* 왼쪽: 로고 */}
-      <div className="header-left">
-        <img
-          className="logo"
-          src="/assets/dog-icon.png"
-          alt="로고"
-        />
+      <div className="flex items-center">
+        <Link to="/">
+          <img
+            className="w-10 h-10"
+            src="/assets/dog-icon.png"
+            alt="로고"
+          />
+        </Link>
       </div>
 
       {/* 중앙: 메뉴 */}
-      <nav className="header-nav">
-        <ul>
-          <li>보호소 찾기</li>
-          <li>라이브</li>
-          <li>후원하기</li>
-          <li>소식</li>
-        </ul>
+      <nav className="flex space-x-8 text-lg font-medium">
+        <Link
+          to="/shelters"
+          className="text-gray-700 hover:text-black transition"
+        >
+          보호소 찾기
+        </Link>
+        <Link
+          to="/live"
+          className="text-gray-700 hover:text-black transition"
+        >
+          라이브
+        </Link>
+        <Link
+          to="/donate"
+          className="text-gray-700 hover:text-black transition"
+        >
+          후원하기
+        </Link>
+        <Link
+          to="/news"
+          className="text-gray-700 hover:text-black transition"
+        >
+          소식
+        </Link>
       </nav>
 
-      {/* 오른쪽: 검색아이콘 + 로그인/회원가입 + 후원하기 버튼 */}
-      <div className="header-right">
+      {/* 오른쪽: 검색 + 로그인/회원가입 + 후원하기 버튼 */}
+      <div className="flex items-center space-x-4">
         {/* 검색 아이콘 */}
-        <button className="icon-button search">
-          <img src="/assets/search-icon.png" alt="검색" />
+        <button className="p-2">
+          <img
+            src="/assets/search-icon.png"
+            alt="검색"
+            className="w-5 h-5"
+          />
         </button>
 
-        {/* 로그인 / 회원가입 텍스트 */}
-        <div className="login-join">
-          <span>로그인</span>
-          <span className="divider"> | </span>
-          <span>회원가입</span>
+        {/* 로그인/회원가입 */}
+        <div className="flex items-center text-gray-700 space-x-2">
+          <Link
+            to="/login"
+            className="hover:text-black transition"
+          >
+            로그인
+          </Link>
+          <span className="text-gray-400">|</span>
+          <Link
+            to="/signup"
+            className="hover:text-black transition"
+          >
+            회원가입
+          </Link>
         </div>
 
         {/* 후원하기 버튼 */}
-        <button className="donate-button">후원하기</button>
+        <Link to="/donate">
+          <button className="px-4 py-2 bg-red-500 text-white rounded-lg shadow-md hover:bg-red-600 transition">
+            후원하기
+          </button>
+        </Link>
       </div>
     </header>
   );
