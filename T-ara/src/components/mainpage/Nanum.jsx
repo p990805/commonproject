4d72640ed90a11f3a8ca02f4d4388886ed183013
박주찬 <p990805@naver.com>
2025-01-29 23:30:56 +0900

    [주찬] Fix : 스크롤 2개 생기는 오류 해결 01.29

--- T-ara/src/components/mainpage/Nanum.jsx
+++ T-ara/src/components/mainpage/Nanum.jsx
@@ -1,4 +1,5 @@
 import SwiperComponent from "./SwiperComponent";
+import { Link } from "react-router-dom";
 
 const Nanum = () => {
   return (
@@ -9,26 +10,8 @@ const Nanum = () => {
           <h2 className="text-3xl font-bold text-gray-800">나눔이야기</h2>
           <p className="text-gray-500">유기동물과 함께한 추억과 후원자님들의 이야기를 전합니다.</p>
         </div>
-        <a
-          href="#"
-          className="text-red-500 font-medium hover:underline flex items-center space-x-1"
-        >
-          <span>더보기</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-4 w-4"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M12 4v16m8-8H4"
-            />
-          </svg>
-        </a>
+        <Link to="/community"
+        className="text-red-500 font-bold hover:underline flex items-center space-x-1" >더보기+</Link>
       </div>
 
       {/* Swiper 컴포넌트 */}
